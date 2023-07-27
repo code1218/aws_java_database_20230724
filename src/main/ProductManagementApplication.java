7a70f3ad55f6eefc648682577cc0f1dba0e7e6bf
devgiants75 <devgiants75@naver.com>
2023-07-27 09:33:17 +0900

    product 시작

--- src/main/ProductManagementApplication.java
+++ src/main/ProductManagementApplication.java
@@ -6,6 +6,12 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
+import frame.ProductRegisterFrame;
+
+import javax.swing.JButton;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
 public class ProductManagementApplication extends JFrame {
 
 	private JPanel contentPane;
@@ -30,6 +36,21 @@ public class ProductManagementApplication extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JButton productRegisterFrameOpenButton = new JButton("상품등록");
+		productRegisterFrameOpenButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				ProductRegisterFrame productRegisterFrame = new ProductRegisterFrame();
+				productRegisterFrame.setVisible(true);
+			}
+		});
+		productRegisterFrameOpenButton.setBounds(85, 111, 97, 23);
+		contentPane.add(productRegisterFrameOpenButton);
+		
+		JButton productListFrameOpenButton = new JButton("상품조회");
+		productListFrameOpenButton.setBounds(251, 111, 97, 23);
+		contentPane.add(productListFrameOpenButton);
 	}
-
 }
